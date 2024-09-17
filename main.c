875a8e384fb1501fe6e8808badfe2cb9bc8b31aa
Samarth Sanjay Pyati <samarthpyati@gmail.com>
2024-09-18 01:06:45 +0530

    nob now rebuild all the stb libs and prints execution time, also tried to refactor the seam removing code and optimized by not computing luminance every_time

--- main.c
+++ main.c
@@ -41,29 +41,29 @@ typedef struct
 }Img;
 
 
-#define mat_at(mat, i, j) ((mat).items[(mat).width * (i) + (j)])
-#define img_at(img, i, j) ((img).pixels[(img).width * (i) + (j)])
+#define mat_at(mat, i, j) ((mat).items[(mat).stride * (i) + (j)])
+#define img_at(img, i, j) ((img).pixels[(img).stride * (i) + (j)])
 
 Mat mat_alloc(int width, int height)
 {
-    Mat m = {0};
-    m.items = (float *) malloc(sizeof(*m.items) * width * height);
-    assert(m.items != NULL);    
-    m.height = height;
-    m.width = width;
-    m.stride = width;
-    return m;
+    Mat mat = {0};
+    mat.items = (float *) malloc(sizeof(*mat.items) * width * height);
+    assert(mat.items != NULL);    
+    mat.height = height;
+    mat.width = width;
+    mat.stride = width;
+    return mat;
 }
 
 Img img_alloc(int width, int height)
 {
-    Img im = {0};
-    im.pixels = (uint32_t *) malloc(sizeof(*im.pixels) * width * height);
-    assert(im.pixels != NULL);
-    im.height = height;
-    im.width = width;
-    im.stride = width;
-    return im;
+    Img img = {0};
+    img.pixels = (uint32_t *) malloc(sizeof(*img.pixels) * width * height);
+    assert(img.pixels != NULL);
+    img.height = height;
+    img.width = width;
+    img.stride = width;
+    return img;
 }
 
 void mat_free(Mat *mat)
@@ -75,13 +75,6 @@ void img_free(Img *img)
 {
     free(img->pixels);
 }
-
-
-#define SPDA_ARR_LEN(xs) sizeof(xs) / sizeof(xs[0])
-#define raise(err, type)                                        \
-    do {                                                        \
-        fprintf(stderr, "ERROR {%s} : %s\n", err, type);        \
-    } while (0)
 
 #define RED (uint32_t)0xFF0000FF;
 
@@ -117,28 +110,17 @@ float rgb_to_lum(uint32_t rgb)
     return (0.2126 * r + 0.7152 * g + 0.0722 * b);
 }
 
-void luminance(Img img, Mat lum)
-{   
-    assert(lum.items != NULL);
-    assert(lum.width == img.width);
-    assert(lum.height == img.height);
-
-    for (int y = 0; y < img.height; ++y)
-    {
-        for (int x = 0; x < img.width; ++x) {
-            mat_at(lum, y, x) = rgb_to_lum(img_at(img, y, x));
-        }
-    }
-}
-
 void min_and_max(Mat *values, float *min, float *max)
 {
     *min = FLT_MAX;
     *max = FLT_MIN;
-    for (int i = 0; i < (values->width * values->height); ++i)
+    for (int y = 0; y < values->height; ++y)
     {
-        if (values->items[i] < *min) *min = values->items[i];
-        if (values->items[i] > *max) *max = values->items[i];
+        for (int x = 0; x < values->width; ++x)
+        {
+            if (mat_at(*values, y, x) < *min) *min = mat_at(*values, y, x);
+            if (mat_at(*values, y, x) > *max) *max = mat_at(*values, y, x);
+        }
     }
 }
 
@@ -196,12 +178,26 @@ void dump_img(const char *fp, Img img)
     // Dumps the normalized images i.e with float values from 0.0 to 1.0 to a file path 
     assert(img.pixels != NULL);
 
-    if (!stbi_write_png(fp, img.width, img.height, 4, img.pixels, img.stride * sizeof(*img.pixels)))  
+    if (!stbi_write_png(fp, img.width, img.height, 4, img.pixels, img.stride * sizeof(uint32_t)))  
     {
         fprintf(stderr, "ERROR: Could not write the image file %s\n", fp);
         exit(EXIT_FAILURE);
     }
     else fprintf(stderr, "OK: Saved the image file at %s\n", fp);
+}
+
+void luminance(Img img, Mat lum)
+{   
+    assert(lum.items != NULL);
+    assert(lum.width == img.width);
+    assert(lum.height == img.height);
+
+    for (int y = 0; y < lum.height; ++y)
+    {
+        for (int x = 0; x < lum.width; ++x) {
+            mat_at(lum, y, x) = rgb_to_lum(img_at(img, y, x));
+        }
+    }
 }
 
 void sobel(Mat lum, Mat grad) 
@@ -213,25 +209,21 @@ void sobel(Mat lum, Mat grad)
     assert(lum.height == grad.height);
 
     static float gx[3][3] = {
-        {1, 0, -1},
-        {2, 0, -2},
-        {1, 0, -1}
+        {1.0, 0.0, -1.0},
+        {2.0, 0.0, -2.0},
+        {1.0, 0.0, -1.0}
     };
 
     static float gy[3][3] = {
-        {1, 2, 1},
-        {0, 0, 0},
-        {-1, -2, -1}
+        {1.0, 2.0, 1.0},
+        {0.0, 0.0, 0.0},
+        {-1.0, -2.0, -1.0}
     };
-
-    // gradient or edge mapping of image
-    int width = lum.width;
-    int height = lum.height;
     
     // Convolving the kernel and the image
-    for (int cy = 0; cy < height; ++cy)
+    for (int cy = 0; cy < lum.height; ++cy)
     {
-        for (int cx = 0; cx < width; ++cx)
+        for (int cx = 0; cx < lum.width; ++cx)
         {
             float sx = 0.0f;
             float sy = 0.0f;
@@ -242,7 +234,7 @@ void sobel(Mat lum, Mat grad)
                     int x = cx + kx;
                     int y = cy + ky;
                     // ensure x & y stay in bounds of image dimensions
-                    float l = (0 <= x && x < width && 0 <= y && y < height) ? mat_at(lum, y, x) : 0.0f;
+                    float l = (0 <= x && x < lum.width && 0 <= y && y < lum.height) ? mat_at(lum, y, x) : 0.0f;
                     // kx and ky start from -1 so offset by 1
                     sx += gx[ky + 1][kx + 1] * l;
                     sy += gy[ky + 1][kx + 1] * l;
@@ -260,18 +252,16 @@ void energy(Mat grad, Mat energy)
     assert(grad.width == energy.width);
     assert(grad.height == energy.height);
 
-    int width = energy.width;
-    int height = energy.height;
     // Calculate the energy 
-    for (int x = 0; x < width; ++x) 
+    for (int x = 0; x < grad.width; ++x) 
     {   
         // Copy the first row as it is, as there's no row above to calculate the energy
         mat_at(energy, 0, x) = mat_at(grad, 0, x);
     }
     
-    for (int cy = 1; cy < height; ++cy)
+    for (int cy = 1; cy < grad.height; ++cy)
     {
-        for (int cx = 0; cx < width; ++cx)
+        for (int cx = 0; cx < grad.width; ++cx)
         {       
             // M(i, j) = e(i, j) + min(M(i − 1, j − 1), M(i − 1, j), M(i − 1, j + 1))
             float min = FLT_MAX;
@@ -279,7 +269,7 @@ void energy(Mat grad, Mat energy)
             {
                 int x = cx + dx;
                 // float value = (0 <= x && x < width) ? energy[(cy - 1) * width + x] : 0.0f;
-                float value = (0 <= x && x < width) ? mat_at(energy, (cy - 1), x) : FLT_MAX;
+                float value = (0 <= x && x < grad.width) ? mat_at(energy, (cy - 1), x) : FLT_MAX;
                 // Why FLT_MAX?: to not make it seam as minimum energy seam
                 if (value < min)        min = value;
             }
@@ -301,11 +291,11 @@ Mat gaussianBlur(Mat lum)
     };  // (/ 16) divide every element by 16
 #else 
     static float bk5[5][5] = {      // blur kernel (5 x 5) gaussian blur (better approx)
-        {1, 4 , 6 , 4 , 1},
-        {4, 16, 24, 16, 4},
-        {6, 24, 36, 24, 6},
-        {4, 16, 24, 16, 4},
-        {1, 4 , 6 , 4 , 1}
+        {1.0, 4.0 , 6.0 , 4.0 , 1.0},
+        {4.0, 16.0, 24.0, 16.0, 4.0},
+        {6.0, 24.0, 36.0, 24.0, 6.0},
+        {4.0, 16.0, 24.0, 16.0, 4.0},
+        {1.0, 4.0 , 6.0 , 4.0 , 1.0}
     };  // (/ 256) divide every element by 256
 #endif 
 
@@ -343,30 +333,34 @@ Mat gaussianBlur(Mat lum)
     return blur;
 }
 
+void remove_pixel_img(Img img, int r, int c)
+{   
+    // Remove a particular pixel from image ((r, c) pixel coordinate)
+    uint32_t *pixels_row = &img_at(img, r, 0);   
+    memmove(pixels_row + c, pixels_row + c + 1, ((img.width - c - 1) * sizeof(uint32_t)));      
+}
 
-// Implementing bit masking 
-typedef enum {
-    DUMP_DUMP_IMg    = 0x00000400,  // 1 << 10
-    DUMP_LUMINANCE     = 0x00000001,  // 1 << 0
-    DUMP_GRADIENT      = 0x00000002,  // 1 << 1
-    DUMP_ENERGY        = 0x00000004,  // 1 << 2
-    DUMP_GAUSSIAN_BLUR = 0x00000008,  // 1 << 3
-    DUMP_COUNTS        = 0x00000010   // 1 << 4
-} dump_modes;
+void remove_pixel_mat(Mat mat, int r, int c)
+{   
+    // Remove a particular pixel from image ((r, c) pixel coordinate)
+    float *pixels_row = &mat_at(mat, r, 0);   
+    memmove(pixels_row + c, pixels_row + c + 1, ((mat.width - c - 1) * sizeof(float)));      
+}
 
 int main(int argc, char *argv[])
 {       
-   if (argc != 2)
+   if (argc != 3)
     {
-        fprintf(stderr, "USAGE: <c_exec_file> <image_path (.jpgs file only)>\n");
+        fprintf(stderr, "USAGE: <c_exec_file> <image_path (.jpgs file only)> <seams_to_remove>\n");
         exit(EXIT_FAILURE);
     }
 
     const char *inp_img_fp = argv[1];
+    size_t seam_to_remove = atoi(argv[2]);
 
     int _width, _height;
-    uint32_t *pixels = (uint32_t *) stbi_load(inp_img_fp, &_width, &_height, NULL, 4);
-    if (!pixels)  
+    uint32_t *pixels_ = (uint32_t *) stbi_load(inp_img_fp, &_width, &_height, NULL, 4);
+    if (!pixels_)  
     {
         fprintf(stderr, "ERROR: Failed to load image %s\n", inp_img_fp);
         exit(EXIT_FAILURE);
@@ -375,15 +369,17 @@ int main(int argc, char *argv[])
     Img img = {
         .height = _height,
         .width = _width,
-        .pixels = pixels,
+        .pixels = pixels_,
         .stride = _width
     };
 
     Mat lum = mat_alloc(_width, _height);
     Mat grad = mat_alloc(_width, _height);
     Mat egy = mat_alloc(_width, _height);  
+    
+    luminance(img, lum);
 
-    // Iteration the rows from top to bottom and find the lowest energy pixel 
+    // Iteration the rows from top to bottom and find the lowest energy pixel   
     // or just pick a random seam from the vicinity
     // i.e, the three neighbouring images on top of the low energy pixel 
 
@@ -393,34 +389,39 @@ int main(int argc, char *argv[])
     // - pixels_row + seam + 1 points to next pixel of 'f'
     // - copying entire array of &(pixels_row + seam + 1) to 1 step back and then reduce width
 
-    size_t n_iters = 100;
-    for (size_t i = 0; i < n_iters; ++i) {
+    for (size_t i = 0; i < seam_to_remove; ++i) {
         printf("Removing Seam %zu\n", i);
        
         // Update these at every iteration 
-        luminance(img, lum);
-        sobel(lum, grad);
+        sobel(lum, grad);   
         energy(grad, egy);
 
         // Finding the minimum energy seam or selecting a random one
-        int seam = (rand() % img.width);
         int y = img.height - 1;  
+        int seam = 0;
+        for (int x = 0; x < img.width; ++x) 
+        {
+            if (mat_at(egy, y, x) < mat_at(egy, y, seam)) seam = x;
+        }
 
-        uint32_t *pixels_row = &img_at(img, y, 0);   // get the last row of the image
-        memmove(pixels_row + seam, pixels_row + seam + 1, ((img.width - seam - 1) * sizeof(uint32_t)));       // removing the seam 
+        remove_pixel_img(img, y, seam);
+        // Since lum is one to one mapping of image we can just remove the pixels instead of 
+        // recomputing the lum everytime
+        remove_pixel_mat(lum, y, seam);
 
         for (y = img.height - 2; y >= 0; --y)
-        {   
+        {       
+            int original_seam = seam;
             for (int dx = -1; dx <= 1; ++dx)
             {
-                int x = seam + dx;                 // find the appropriate seam col
+                int x = original_seam + dx;                 
                 if (0 <= x && x < img.width && mat_at(egy, y, x) < mat_at(egy, y, seam))
                 {   
                     seam = x;
                 }
             }
-            pixels_row = &img_at(img, y, 0);                                                         // get the row of the image
-            memmove(pixels_row + seam, pixels_row + seam + 1, ((img.width - seam - 1) * sizeof(uint32_t)));    // remove the seam 
+            remove_pixel_img(img, y, seam);
+            remove_pixel_mat(lum, y, seam);
             // seam_image.pixels[y * seam_image.width + seam] = RED;     // Visualize the seams
         }
 
@@ -431,9 +432,9 @@ int main(int argc, char *argv[])
         egy.width  -= 1;  
     }   
 
-    dump_img("seam.png", img);
+    dump_img("output.png", img);
 
-    stbi_image_free(pixels);
+    stbi_image_free(pixels_);
     mat_free(&lum);
     mat_free(&egy);
     mat_free(&grad);
--- nob.c
+++ nob.c
@@ -1,28 +1,79 @@
+#include <assert.h>
+
 #define NOB_IMPLEMENTATION
 #include "external/nob.h"
 
+#include <time.h>
 
-#define COMPILE_FASTER 
+// #define COMPILE_FASTER
 
-int main(int argc, char *argv[])
-{   
-    NOB_GO_REBUILD_URSELF(argc, argv);
-    Nob_Cmd cmd = {0};
-    nob_cmd_append(&cmd, "cc");
-    nob_cmd_append(&cmd, "-Wall", "-Wextra", "-ggdb", "-std=c17");
+double get_time(void)
+{
+    struct timespec tp = {};
+    int ret = clock_gettime(CLOCK_MONOTONIC, &tp);
+    assert(ret == 0);
+    return tp.tv_sec + tp.tv_nsec * 1e-9;
+}
 
-#if defined(COMPILE_FASTER)
-    nob_cmd_append(&cmd, "-O3");
+void cc(Nob_Cmd *cmd)
+{
+    nob_cmd_append(cmd, "cc");
+    nob_cmd_append(cmd, "-Wall", "-Wextra", "-ggdb");
+#if !defined(COMPILE_FASTER)
+    nob_cmd_append(cmd, "-O3");
 #else 
-    nob_cmd_append(&cmd, "-O0");
+    nob_cmd_append(cmd, "-O0");
 #endif
-    nob_cmd_append(&cmd, "-o", "main");
-    nob_cmd_append(&cmd, "stb_image.o", "stb_image_write.o", "nob.o", "main.c");
+}
 
-    if (!nob_cmd_run_sync(cmd)) return -1;
+bool rebuild_stb_if_needed(Nob_Cmd *cmd, const char *implementation, const char *input, const char *output)
+{
+    if (nob_needs_rebuild1(output, input)) {
+        cmd->count = 0;
+        cc(cmd);
+        nob_cmd_append(cmd, implementation);
+        nob_cmd_append(cmd, "-x", "c");
+        nob_cmd_append(cmd, "-c");
+        nob_cmd_append(cmd, "-o", output);
+        nob_cmd_append(cmd, input);
+        return nob_cmd_run_sync(*cmd);
+    } else {
+        nob_log(NOB_INFO, "%s is up to date", output);
+        return true;
+    }
+}
 
-    // cmd.count = 0;
-    // nob_cmd_append(&cmd, "./main");
-    // if (!nob_cmd_run_sync(cmd)) return -1;
+int main(int argc, char **argv)
+{
+    NOB_GO_REBUILD_URSELF(argc, argv);
+
+    const char *program = nob_shift_args(&argc, &argv);
+    (void) program;
+
+    Nob_Cmd cmd = {0};
+
+    if (!nob_mkdir_if_not_exists("./build/")) return 1;
+    if (!rebuild_stb_if_needed(&cmd, "-DSTB_IMAGE_IMPLEMENTATION", "external/stb_image.h", "./build/stb_image.o")) return 1;
+    if (!rebuild_stb_if_needed(&cmd, "-DSTB_IMAGE_WRITE_IMPLEMENTATION", "external/stb_image_write.h", "./build/stb_image_write.o")) return 1;
+
+    const char *main_input = "main.c";
+    const char *main_output = "./build/main";
+    cmd.count = 0;
+    cc(&cmd);
+    nob_cmd_append(&cmd, "-o", main_output);
+    nob_cmd_append(&cmd, main_input);
+    nob_cmd_append(&cmd, "./build/stb_image.o");
+    nob_cmd_append(&cmd, "./build/stb_image_write.o");
+    nob_cmd_append(&cmd, "-lm");
+    if (!nob_cmd_run_sync(cmd)) return 1;
+
+    cmd.count = 0;
+    nob_cmd_append(&cmd, main_output);
+    nob_da_append_many(&cmd, argv, argc);
+    double begin = get_time();
+    if (!nob_cmd_run_sync(cmd)) return 1;
+    double end = get_time();
+    nob_log(NOB_INFO, "Execution time: %lf(s)", end - begin);
+
     return 0;
 }
