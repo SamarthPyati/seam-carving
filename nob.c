64609c328a414cbb663991433912d406762a91d0
Samarth Sanjay Pyati <samarthpyati@gmail.com>
2026-08-03 16:08:46 +0530

    Update nob.h

--- nob.c
+++ nob.c
@@ -1,11 +1,12 @@
 #include <assert.h>
 
+#define NOB_STRIP_PREFIX
 #define NOB_IMPLEMENTATION
 #include "external/nob.h"
 
 #include <time.h>
 
-// #define COMPILE_FASTER
+// #define NO_OPTIMISATION
 #define COMPILE_NATIVE
 
 static double get_time(void)
@@ -16,48 +17,48 @@ static double get_time(void)
     return tp.tv_sec + tp.tv_nsec * 1e-9;
 }
 
-static void cc(Nob_Cmd *cmd)
+static void cc(Cmd *cmd)
 {
-    nob_cmd_append(cmd, "cc");
-    nob_cmd_append(cmd, "-Wall", "-Wextra", "-Wno-unused-function", "-g", "-std=c17");
-    nob_cmd_append(cmd, "-fsanitize=address,undefined");
+    cmd_append(cmd, "cc");
+    cmd_append(cmd, "-Wall", "-Wextra", "-Wno-unused-function", "-g", "-std=c17");
+    cmd_append(cmd, "-fsanitize=address,undefined");
 
 #if defined(COMPILE_NATIVE)
-    nob_cmd_append(cmd, "-march=native");
+    cmd_append(cmd, "-march=native");
 #endif
 
-#if !defined(COMPILE_FASTER)
-    nob_cmd_append(cmd, "-O3");
+#if !defined(NO_OPTIMISATION)
+    cmd_append(cmd, "-O3");
 #else
-    nob_cmd_append(cmd, "-O0");
+    cmd_append(cmd, "-O0");
 #endif
 }
 
 static bool build_binary(Nob_Cmd *cmd, const char *source, const char *output)
 {
-    if (nob_needs_rebuild1(output, source)) {
+    if (needs_rebuild1(output, source)) {
         cmd->count = 0;
         cc(cmd);
-        nob_cmd_append(cmd, "-I./include");
-        nob_cmd_append(cmd, "-I./external");
-        nob_cmd_append(cmd, "-o", output);
-        nob_cmd_append(cmd, source);
-        nob_cmd_append(cmd, "-lm");
-        return nob_cmd_run_sync(*cmd);
+        cmd_append(cmd, "-I./include");
+        cmd_append(cmd, "-I./external");
+        cmd_append(cmd, "-o", output);
+        cmd_append(cmd, source);
+        cmd_append(cmd, "-lm");
+        return cmd_run(cmd);
     }
 
-    nob_log(NOB_INFO, "%s is up to date", output);
+    nob_log(INFO, "%s is up to date", output);
     return true;
 }
 
 int main(int argc, char **argv)
 {
-    NOB_GO_REBUILD_URSELF(argc, argv);
+    GO_REBUILD_URSELF(argc, argv);
 
-    const char *program = nob_shift_args(&argc, &argv);
+    const char *program = shift_args(&argc, &argv);
     (void)program;
 
-    Nob_Cmd cmd = {0};
+    Cmd cmd = {0};
 
     if (!nob_mkdir_if_not_exists("./build/")) return 1;
 
@@ -66,12 +67,12 @@ int main(int argc, char **argv)
     if (!build_binary(&cmd, main_source, main_output)) return 1;
 
     cmd.count = 0;
-    nob_cmd_append(&cmd, main_output);
-    nob_da_append_many(&cmd, argv, argc);
+    cmd_append(&cmd, main_output);
+    da_append_many(&cmd, argv, argc);
     double begin = get_time();
     if (!nob_cmd_run_sync(cmd)) return 1;
     double end = get_time();
-    nob_log(NOB_INFO, "Execution time: %lf second(s)", end - begin);
+    nob_log(INFO, "Execution time: %lf second(s)", end - begin);
 
     return 0;
 }
